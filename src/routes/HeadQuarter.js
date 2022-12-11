801c2ed4cd0d6d7701d8b06013bc7b93534ded14
20021341 <20021341@vnu.edu.vn>
2022-12-11 22:11:33 +0700

    update

--- src/routes/HeadQuarter.js
+++ src/routes/HeadQuarter.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { Redirect, Route, Switch } from 'react-router-dom';
 import FacilityManage from '../containers/HeadQuarter/FacilityManage';
-import CustomerMangage from '../containers/HeadQuarter/CustomerMangage';
+import CustomerManage from '../containers/HeadQuarter/CustomerManage';
 import ProductLineManage from '../containers/HeadQuarter/ProductLineManage';
 
 class HeadQuarter extends Component {
@@ -12,7 +12,7 @@ class HeadQuarter extends Component {
                 <div className="system-list">
                     <Switch>
                         <Route path="/hq/facility-manage" component={FacilityManage} />
-                        <Route path="/hq/customer-manage" component={CustomerMangage} />
+                        <Route path="/hq/customer-manage" component={CustomerManage} />
                         <Route path="/hq/product-line-manage" component={ProductLineManage} />
                         <Redirect to="/" />
                     </Switch>
